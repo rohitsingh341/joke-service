30cf65b9ff95f21fc3796c43a4488e65f0b9dbab
rohitsingh <rohitsingh341@gmail.com>
2020-05-30 20:09:00 +0530

    Utilising @Configuration annotation for creating a bean of external class or a third party class.

---
+++ src/main/java/com/psr/sbootdemo/config/ChuckConfig.java
@@ -0,0 +1,15 @@
+package com.psr.sbootdemo.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChuckConfig {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+
+}
--- src/main/java/com/psr/sbootdemo/services/JokesServiceImpl.java
+++ src/main/java/com/psr/sbootdemo/services/JokesServiceImpl.java
@@ -7,6 +7,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class JokesServiceImpl implements JokesService {
 
+    private ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokesServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
+
     @Override
     public String getJoke() {
         return new ChuckNorrisQuotes().getRandomQuote();
