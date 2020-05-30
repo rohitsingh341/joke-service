0c3bc636bedd30d0edefbd2cef56118f6509e478
rohitsingh <rohitsingh341@gmail.com>
2020-05-30 20:32:32 +0530

    Using XML configuration to expose bean of third party class.

--- src/main/java/com/psr/sbootdemo/SbootDemoApplication.java
+++ src/main/java/com/psr/sbootdemo/SbootDemoApplication.java
@@ -3,9 +3,11 @@ package com.psr.sbootdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"config", "com.psr.sbootdemo"})
+@ImportResource("classpath:chuck-config.xml")
 public class SbootDemoApplication {
 
     public static void main(String[] args) {
